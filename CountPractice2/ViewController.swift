e1745893a02194807520b324a283ae58dd42335b
Mizuki Morohoshi <8080suisho@gmail.com>
2020-08-25 03:02:31 +0900

    Countアプリの完成

--- CountPractice2/ViewController.swift
+++ CountPractice2/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label : UILabel!
+    
+    var number : Int = 0
+    
+    func colorControll(){
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number < 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func plus(){
+        number += 1
+        label.text = String(number)
+        
+        colorControll()
+        
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+        
+        colorControll()
+    }
+    
+    @IBAction func kakeru(){
+        number *= 2
+        label.text = String(number)
+        
+        colorControll()
+    }
+    
+    @IBAction func waru(){
+        number /= 2
+        label.text = String(number)
+        
+       colorControll()
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        
+        colorControll()
+        
+    }
+    
+    
 
 }
 
